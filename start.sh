215c690fba49b5149a649efb16e0956e9dc52af5
stamaimer <stamaimer@gmail.com>
2017-03-16 11:46:05 +0800

    change start configuration

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 
 source venv/bin/activate
 
-gunicorn -w 3 -k gevent run:app -p app.pid -b 127.0.0.1:7070 --log-level=DEBUG --access-logfile log/access_log --error-logfile log/error_log --preload
+gunicorn -w 9 -k gevent run:app -p app.pid -b 0.0.0.0:7070 --log-level=DEBUG --access-logfile log/access_log --error-logfile log/error_log --preload
