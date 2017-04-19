2ace8a660d0e06cda5eb030ca061ae2c7ac47ac8
stamaimer <stamaimer@gmail.com>
2017-04-19 17:35:30 +0800

    add plan_hour to list view of project & filter None value of loan notation

--- app/admin/project.py
+++ app/admin/project.py
@@ -23,7 +23,7 @@ class ProjectModelView(AppModelView):
 
     column_searchable_list = ["no", "name", "current_stage.text", "charge_user.username", "region.text", "status"]
 
-    column_list = ["no", "name", "current_stage", "charge_user", "region", "status", "create_datetime"]
+    column_list = ["no", "name", "current_stage", "charge_user", "region", "status", "plan_hour", "create_datetime"]
 
     column_details_list = ["no", "name", "current_stage", "charge_user", "region", "status", "plan_hour",
                            "create_datetime", "update_datetime"]
